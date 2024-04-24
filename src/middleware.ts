6abaee531f7da08b6def646229475608ce55778f
Mees Boeijen <mees@amees.me>
2024-04-24 11:19:22 +0200

    Lomo-redirect

---
+++ src/middleware.ts
@@ -0,0 +1,22 @@
+import type { NextRequest } from "next/server";
+import { NextResponse, userAgent } from "next/server";
+
+export function middleware(req: NextRequest) {
+  const { ua } = userAgent(req);
+
+  if (/iP(hone|ad|od)/.test(ua)) {
+    return NextResponse.redirect(
+      "https://apps.apple.com/nl/app/lomo-disposable-camera/id6468936854"
+    );
+  } else if (/Android/.test(ua)) {
+    return NextResponse.redirect(
+      "https://play.google.com/store/apps/details?id=co.lomoapp"
+    );
+  } else {
+    return NextResponse.redirect("https://lomoapp.co/");
+  }
+}
+
+export const config = {
+  matcher: "/",
+};
