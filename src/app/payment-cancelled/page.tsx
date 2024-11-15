80f4db49bfdac049014cfc125edbfd9b41981e44
Mees Boeijen <mees@amees.me>
2024-11-15 17:24:23 +0100

    Different redirect

--- src/app/payment-cancelled/page.tsx
+++ src/app/payment-cancelled/page.tsx
@@ -5,7 +5,7 @@ import { useEffect } from "react";
 export default function PaymentCancelledRoute() {
   useEffect(() => {
     setTimeout(() => {
-      window.location.href = "co.lomoapp://payment?status=cancelled";
+      window.location.href = "co.lomoapp://?status=cancelled";
     }, 200);
   }, []);
 
--- src/app/payment-success/page.tsx
+++ src/app/payment-success/page.tsx
@@ -5,7 +5,7 @@ import { useEffect } from "react";
 export default function PaymentSuccessRoute() {
   useEffect(() => {
     setTimeout(() => {
-      window.location.href = "co.lomoapp://payment?status=success";
+      window.location.href = "co.lomoapp://?status=success";
     }, 200);
   }, []);
 
